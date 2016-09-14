04ab427270e85dc7df4ab394921c265eb260073c
Tyler Smith <tyler@plantarum.ca>
2016-09-14 16:20:32 -0400

    Added names to flowPloidyFiles vector

--- R/setup.R
+++ R/setup.R
@@ -1,3 +1,4 @@
 flowPloidyFiles <- 
   list.files(system.file("extdata/", package = "flowPloidyData"), 
              pattern = "*.LMD", full.names = TRUE)
+names(flowPloidyFiles) <- basename(flowPloidyFiles)
